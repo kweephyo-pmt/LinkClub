aed43ba9c2ea5c98a82066dc3c6e95092ede4d4f
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2025-08-29 01:13:41 +0700

    Minor fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
         <Route path="/" element={authUser ? <HomePage /> : <LandingPage />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/settings" element={authUser ? <SettingsPage /> : <Navigate to="/login" />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
         <Route path="/friends" element={authUser ? <FriendsPage /> : <Navigate to="/login" />} />
       </Routes>
