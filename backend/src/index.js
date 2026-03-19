952e9c59a4933caf7eabd12a68a4a242607e20fd
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2026-03-19 11:50:40 +0700

    minor fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,10 +33,15 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/friends", friendRoutes);
 app.use("/api/stream", streamRoutes);
 
-app.get("/", (req, res) => {
-  res.send("LinkClub API is running!");
-});
-server.listen(PORT, "0.0.0.0", () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
